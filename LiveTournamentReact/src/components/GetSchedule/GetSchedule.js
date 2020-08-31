cca50d99b01f5b3526f23ea1cc556419978ac656
Subash Senthil Kumar <65695964+subashss10@users.noreply.github.com>
2020-08-31 20:01:21 +0530

    Modified GetSchedule

--- LiveTournamentReact/src/components/GetSchedule/GetSchedule.js
+++ LiveTournamentReact/src/components/GetSchedule/GetSchedule.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-import { Container, Jumbotron} from 'react-bootstrap';
+import { Container, Jumbotron, Row,Col} from 'react-bootstrap';
 import { Link } from '@version/react-router-v3';
 import { NavDropdown } from 'react-bootstrap';
 import '@fortawesome/fontawesome-free/css/all.min.css'; 
@@ -12,9 +12,23 @@ class GetSchedule extends React.Component {
     constructor(props) {
         super(props);
         this.getSchedules = this.getSchedules.bind(this);
+        this.getMatchType = this.getMatchType.bind(this);
         this.state = {
             schedules: []
         };
+    }
+
+    getMatchType(type){
+        switch(type){
+            case 1:
+                return 'International';
+            case 2:
+                return 'T20';
+            case 3:
+                return 'Test';
+            default:
+                return 'International';            
+        }
     }
     
 
@@ -54,13 +68,40 @@ class GetSchedule extends React.Component {
     render() {
         return (
             <>
+            <Row><Col xs={3}><Container><Jumbotron>
+                            Date
+
+
+                                </Jumbotron></Container></Col>
+                                <Col xs={6}><Container><Jumbotron>
+                            Match Details
+
+
+                                </Jumbotron></Container></Col>
+                                <Col xs={3}><Container><Jumbotron>
+                                Time
+    
+    
+                                    </Jumbotron></Container></Col>
+                                </Row>
                     {
                         this.state.schedules.map((schedule)=>{
-                        return  <Container><Jumbotron>
+                        return <Row><Col xs={3}><Container><Jumbotron>
                             {schedule.Date}
 
 
-                                </Jumbotron></Container>;})
+                                </Jumbotron></Container></Col>
+                                <Col xs={6}><Container><Jumbotron>
+                            {schedule.Team1 + ' Vs ' + schedule.Team2 + ' at ' + schedule.Venue + ' ( ' + this.getMatchType(schedule.MatchTypeId) + ' )'}
+
+
+                                </Jumbotron></Container></Col>
+                                <Col xs={3}><Container><Jumbotron>
+                                {schedule.Time}
+    
+    
+                                    </Jumbotron></Container></Col>
+                                </Row>;})
                     }
                     {/* <NavDropdown.Item eventKey="4.1"><Link to={{pathname:'NewTeam'}}>Create New Schedule</Link></NavDropdown.Item> */}
                
